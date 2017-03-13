fba1a59cb60b5860299dee9807e89c200e04221e
Samantha Behn <horses4life2011@gmail.com>
2017-03-13 17:56:07 -0400

    added database to program

---
+++ src/main/java/com/company/controller/StationsEntity.java
@@ -0,0 +1,83 @@
+package com.company.controller;
+
+import javax.persistence.*;
+
+/**
+ * Created by Samantha on 3/13/2017.
+ */
+@Entity
+@Table(name = "stations", schema = "qline", catalog = "")
+public class StationsEntity {
+    private int stationId;
+    private String stattionName;
+    private double latitude;
+    private double longitude;
+
+    @Id
+    @Column(name = "stationID", nullable = false)
+    public int getStationId() {
+        return stationId;
+    }
+
+    public void setStationId(int stationId) {
+        this.stationId = stationId;
+    }
+
+    @Basic
+    @Column(name = "stattionName", nullable = false, length = 50)
+    public String getStattionName() {
+        return stattionName;
+    }
+
+    public void setStattionName(String stattionName) {
+        this.stattionName = stattionName;
+    }
+
+    @Basic
+    @Column(name = "latitude", nullable = false, precision = 0)
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    @Basic
+    @Column(name = "longitude", nullable = false, precision = 0)
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        StationsEntity that = (StationsEntity) o;
+
+        if (stationId != that.stationId) return false;
+        if (Double.compare(that.latitude, latitude) != 0) return false;
+        if (Double.compare(that.longitude, longitude) != 0) return false;
+        if (stattionName != null ? !stattionName.equals(that.stattionName) : that.stattionName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = stationId;
+        result = 31 * result + (stattionName != null ? stattionName.hashCode() : 0);
+        temp = Double.doubleToLongBits(latitude);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(longitude);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+}
---
+++ src/main/java/com/company/controller/UserinfoEntity.java
@@ -0,0 +1,105 @@
+package com.company.controller;
+
+import javax.persistence.*;
+
+/**
+ * Created by Samantha on 3/13/2017.
+ */
+@Entity
+@Table(name = "userinfo", schema = "qline", catalog = "")
+public class UserinfoEntity {
+    private int userId;
+    private String userLogin;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    @Id
+    @Column(name = "userID", nullable = false)
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    @Basic
+    @Column(name = "userLogin", nullable = false, length = 45)
+    public String getUserLogin() {
+        return userLogin;
+    }
+
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+    }
+
+    @Basic
+    @Column(name = "password", nullable = false, length = 45)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "firstName", nullable = false, length = 40)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Basic
+    @Column(name = "lastName", nullable = false, length = 50)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Basic
+    @Column(name = "phoneNumber", nullable = false, length = 12)
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserinfoEntity that = (UserinfoEntity) o;
+
+        if (userId != that.userId) return false;
+        if (userLogin != null ? !userLogin.equals(that.userLogin) : that.userLogin != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(that.lastName) : that.lastName != null) return false;
+        if (phoneNumber != null ? !phoneNumber.equals(that.phoneNumber) : that.phoneNumber != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId;
+        result = 31 * result + (userLogin != null ? userLogin.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (phoneNumber != null ? phoneNumber.hashCode() : 0);
+        return result;
+    }
+}
