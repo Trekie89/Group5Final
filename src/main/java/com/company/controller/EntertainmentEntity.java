4a80c56835d72c9ef09549451768315f08fb0468
Samantha Behn <horses4life2011@gmail.com>
2017-03-14 16:43:37 -0400

    added database entities

---
+++ src/main/java/com/company/controller/EntertainmentEntity.java
@@ -0,0 +1,79 @@
+package com.company.controller;
+
+import javax.persistence.*;
+
+/**
+ * Created by Samantha on 3/14/2017.
+ */
+@Entity
+@Table(name = "entertainment", schema = "qline", catalog = "")
+public class EntertainmentEntity {
+    private int identertainment;
+    private String name;
+    private String address;
+    private String website;
+
+    @Id
+    @Column(name = "identertainment", nullable = false)
+    public int getIdentertainment() {
+        return identertainment;
+    }
+
+    public void setIdentertainment(int identertainment) {
+        this.identertainment = identertainment;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, length = 100)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "address", nullable = false, length = 100)
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Basic
+    @Column(name = "website", nullable = true, length = 100)
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EntertainmentEntity that = (EntertainmentEntity) o;
+
+        if (identertainment != that.identertainment) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (address != null ? !address.equals(that.address) : that.address != null) return false;
+        if (website != null ? !website.equals(that.website) : that.website != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = identertainment;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        result = 31 * result + (website != null ? website.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/com/company/controller/FoodEntity.java
@@ -0,0 +1,92 @@
+package com.company.controller;
+
+import javax.persistence.*;
+
+/**
+ * Created by Samantha on 3/14/2017.
+ */
+@Entity
+@Table(name = "food", schema = "qline", catalog = "")
+public class FoodEntity {
+    private int idfood;
+    private String name;
+    private String address;
+    private String website;
+    private int stationId;
+
+    @Id
+    @Column(name = "idfood", nullable = false)
+    public int getIdfood() {
+        return idfood;
+    }
+
+    public void setIdfood(int idfood) {
+        this.idfood = idfood;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, length = 100)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "address", nullable = false, length = 100)
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Basic
+    @Column(name = "website", nullable = true, length = 50)
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    @Basic
+    @Column(name = "stationID", nullable = false)
+    public int getStationId() {
+        return stationId;
+    }
+
+    public void setStationId(int stationId) {
+        this.stationId = stationId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FoodEntity that = (FoodEntity) o;
+
+        if (idfood != that.idfood) return false;
+        if (stationId != that.stationId) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (address != null ? !address.equals(that.address) : that.address != null) return false;
+        if (website != null ? !website.equals(that.website) : that.website != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idfood;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        result = 31 * result + (website != null ? website.hashCode() : 0);
+        result = 31 * result + stationId;
+        return result;
+    }
+}
