b10e257fbd2ed629ef3c12e2a2a4829e44065ebc
Robert Fretter <Robert Fretter>
2017-03-14 09:52:58 -0400

    more face

--- src/main/java/com/company/controller/HomeController.java
+++ src/main/java/com/company/controller/HomeController.java
@@ -11,19 +11,47 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 @Controller
 public class HomeController {
-
     @RequestMapping("/")
 
     public ModelAndView helloWorld()
     {
+        FBConnection fbConnection = new FBConnection();
         return new
-                ModelAndView("mainpage","message","Hello World");
+                ModelAndView("welcome","message",fbConnection.getFBAuthUrl());
 
     }
 
+    @RequestMapping("welcome2")
+
+    public ModelAndView helloWorld2(@RequestParam("code") String code)
+    {
+        if (code == null || code.equals("")){
+            throw new RuntimeException("ERROR: Didn't get code parameter in callback.");
+        }
+        FBConnection fbConnection = new FBConnection();
+        String accessToken = fbConnection.getAccessToken(code);
+
+        FBGraph fbGraph = new FBGraph(accessToken);
+        String graph = fbGraph.getFBGraph();
+        Map<String, String> fbProfileData = fbGraph.getGraphData(graph);
+        String out = "";
+        out = out.concat("<h1>Facebook Login using Java</h1>");
+        //out = out.concat("<h2>Facebook Login using Java</h2>");
+        out = out.concat("<div> Welcome" + fbProfileData.get("name"));
+        out = out.concat("<div> Your Email" + fbProfileData.get("email"));
+        out = out.concat("<div> You are" + fbProfileData.get("gender"));
+
+
+
+
+        return new
+                ModelAndView("welcome2","message",out);
+
+    }
 
     @RequestMapping("plan")
 
