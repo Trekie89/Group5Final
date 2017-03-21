4d26a231caaafc4f02811bc8daad2306a1a03d66
Mike Preston <Mike Preston>
2017-03-21 08:52:45 -0400

    added persistence mapping for lodging

---
+++ src/main/java/com/company/entity/LodgingEntity.java
@@ -0,0 +1,79 @@
+package com.company.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by Mike on 3/21/2017.
+ */
+@Entity
+@Table(name = "lodging", schema = "qline", catalog = "")
+public class LodgingEntity {
+    private int lodgingId;
+    private String name;
+    private String address;
+    private String website;
+
+    @Id
+    @Column(name = "lodgingID", nullable = false)
+    public int getLodgingId() {
+        return lodgingId;
+    }
+
+    public void setLodgingId(int lodgingId) {
+        this.lodgingId = lodgingId;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, length = 75)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "address", nullable = false, length = 75)
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Basic
+    @Column(name = "website", nullable = true, length = 75)
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LodgingEntity that = (LodgingEntity) o;
+
+        if (lodgingId != that.lodgingId) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (address != null ? !address.equals(that.address) : that.address != null) return false;
+        if (website != null ? !website.equals(that.website) : that.website != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = lodgingId;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        result = 31 * result + (website != null ? website.hashCode() : 0);
+        return result;
+    }
+}
